a70f7176c7d172ea5dafd353ccf7a8876edd42db
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-09-28 13:04:15 -0700

    Add HTML shell

---
+++ index.js
@@ -0,0 +1,35 @@
+// Create an array of freelancer objects 
+class Freelancer {
+    constructor(name, occupation, price) {
+        this.name = name;
+        this.occupation = occupation;
+        this.price = price;
+    }
+    // Create render method to render freelance to table of freelancers
+    render() {
+        const 
+    }
+}
+const freelancers = [
+    {
+        name: "Alice",
+        occupation: "Writer",
+        price: 30
+    }, {
+        name: "Bob",
+        occupation: "Teacher",
+        price: 50
+    }, {
+        name: "Carol",
+        occupation: "Programmer",
+        price: 70
+    }
+];
+// Calculate price average by summing up freelancer prices
+const averagePrice =
+// divide by number of freelancers(freelancers array length)
+for (const freelancer of freelancers) {
+    
+}
+// Generate random freelancer at an irregular pace that 
+// draws from an array of names, occupations, and prices
