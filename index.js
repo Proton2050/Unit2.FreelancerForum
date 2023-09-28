8907c54c80201238c25c7bbe63fdea8f72ae872e
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-09-28 14:04:47 -0700

    Add rough render

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-// Create an array of freelancer objects 
-class Freelancer {
-    constructor(name, occupation, price) {
-        this.name = name;
-        this.occupation = occupation;
-        this.price = price;
-    }
-    // Create render method to render freelance to table of freelancers
-    render() {
-        const 
-    }
+// Calculate price average
+const updateAvgPrice = () => {
+    let averagePrice = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0)/freelancers.length;
+    const avgPriceMsg = document.querySelector(".average");
+    avgPriceMsg.textContent = `The average starting price is $${averagePrice}.`;
 }
+// Render freelancers to table using .map to render()
+const render = () => {
+    // Get a reference to the table body
+    const tableBody = document.querySelector("tbody");
+    // Create a new table row element
+    const newRow = document.createElement("tr");
+    // Create new table cell elements for each column in table
+    const nameCell = document.createElement("td");
+    nameCell.classList.add("name");
+    const occupationCell = document.createElement("td");
+    occupationCell.classList.add("occupation");
+    const priceCell = document.createElement("td");
+    priceCell.classList.add("price");
+    // Set text content of each cell to freelancer's data
+    nameCell.textContent = freelancer.name;
+    occupationCell.textContent = freelancer.occupation;
+    priceCell.textContent = freelancer.price;
+    // Append the new table cell elements to the new table row element
+    newRow.appendChild(nameCell);
+    newRow.appendChild(occupationCell);
+    newRow.appendChild(priceCell);
+    // Append new row to table body
+    tableBody.appendChild(newRow);
+}
+// Create an array of freelancer objects 
 const freelancers = [
     {
         name: "Alice",
@@ -25,11 +44,11 @@ const freelancers = [
         price: 70
     }
 ];
-// Calculate price average by summing up freelancer prices
-const averagePrice =
-// divide by number of freelancers(freelancers array length)
-for (const freelancer of freelancers) {
-    
-}
+
 // Generate random freelancer at an irregular pace that 
-// draws from an array of names, occupations, and prices
+// draws from an array of names, occupations, and prices and pushes into freelancers array
+
+// Everytime a freelancer is added, update the average price
+
+updateAvgPrice();
+freelancers.map()
