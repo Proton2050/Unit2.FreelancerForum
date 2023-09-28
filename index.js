88d4e26e42727206fcac18d8a27dc204a2e54c18
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-09-28 14:38:42 -0700

    Working but too many freelancers

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// Calculate price average
-const updateAvgPrice = () => {
+// Calculate average starting price and update message
+function updateAvgPrice() {
     let averagePrice = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0)/freelancers.length;
     const avgPriceMsg = document.querySelector(".average");
     avgPriceMsg.textContent = `The average starting price is $${averagePrice}.`;
 }
-// Render freelancers to table using .map to render()
-const render = () => {
+
+// Render freelancers to table and update price average
+function render(freelancer) {
     // Get a reference to the table body
     const tableBody = document.querySelector("tbody");
     // Create a new table row element
@@ -20,15 +21,152 @@ const render = () => {
     // Set text content of each cell to freelancer's data
     nameCell.textContent = freelancer.name;
     occupationCell.textContent = freelancer.occupation;
-    priceCell.textContent = freelancer.price;
+    priceCell.textContent = `$${freelancer.price}`;
     // Append the new table cell elements to the new table row element
     newRow.appendChild(nameCell);
     newRow.appendChild(occupationCell);
     newRow.appendChild(priceCell);
     // Append new row to table body
     tableBody.appendChild(newRow);
+
+    updateAvgPrice();
 }
-// Create an array of freelancer objects 
+
+// Create Random freelancer generator
+function getNewFreelancer() {
+    const newName = names[Math.floor(Math.random() * names.length)];
+    const newOccupation = occupations[Math.floor(Math.random() * occupations.length)];
+    const newPrice = Math.floor(Math.random() * 200);
+    
+    const newFreelancer = {
+        name: newName,
+        occupation: newOccupation,
+        price: newPrice
+    };
+    
+    freelancers.push(newFreelancer);
+    freelancers.map((freelancer) => render(freelancer));
+
+    if (freelancers.length > 50) {
+        clearInterval(addFreelancerIntervalId);
+    }
+}
+
+// Create bank of freelancer names and occupations
+const names = [
+"Alice",
+"Bob",
+"Carol",
+"Dave",
+"Eve",
+"Frank",
+"George",
+"Hannah",
+"Isaac",
+"Jessica",
+"Kevin",
+"Laura",
+"Michael",
+"Nicole",
+"Oliver",
+"Peter",
+"Quinn",
+"Rachel",
+"Samuel",
+"Taylor",
+"Umair",
+"Victoria",
+"William",
+"Xavi",
+"Yusuf",
+"Zara",
+"Aaron",
+"Brandon",
+"Chloe",
+"Daniel",
+"Emily",
+"Finn",
+"Gabriel",
+"Hazel",
+"Ian",
+"Jack",
+"Kate",
+"Logan",
+"Maya",
+"Noah",
+"Olivia",
+"Patrick",
+"Quinn",
+"Ryan",
+"Sarah",
+"Thomas",
+"Uchenna",
+"Violet",
+"William",
+"Xander",
+"Yasmine",
+"Zachary",
+];
+const occupations = [
+"Accountant",
+"Actor",
+"Architect",
+"Artist",
+"Astronaut",
+"Author",
+"Baker",
+"Barber",
+"Biologist",
+"Bricklayer",
+"Bus driver",
+"Carpenter",
+"Chef",
+"Cleaner",
+"Construction worker",
+"Dentist",
+"Doctor",
+"Electrician",
+"Engineer",
+"Farmer",
+"Firefighter",
+"Flight attendant",
+"Gardener",
+"Graphic designer",
+"Hairdresser",
+"Health care assistant",
+"Journalist",
+"Judge",
+"Lawyer",
+"Librarian",
+"Lifeguard",
+"Mechanic",
+"Model",
+"Musician",
+"Nurse",
+"Optician",
+"Painter",
+"Pharmacist",
+"Photographer",
+"Pilot",
+"Plumber",
+"Police officer",
+"Postman",
+"Programmer",
+"Psychiatrist",
+"Receptionist",
+"Restaurant manager",
+"Sales assistant",
+"Scientist",
+"Secretary",
+"Social worker",
+"Software engineer",
+"Teacher",
+"Taxi driver",
+"Veterinarian",
+"Web developer",
+];
+
+// Seed an array of freelancer objects 
 const freelancers = [
     {
         name: "Alice",
@@ -45,10 +183,8 @@ const freelancers = [
     }
 ];
 
-// Generate random freelancer at an irregular pace that 
-// draws from an array of names, occupations, and prices and pushes into freelancers array
+// Populate table with freelancers
+freelancers.map((freelancer) => render(freelancer));
 
-// Everytime a freelancer is added, update the average price
-
-updateAvgPrice();
-freelancers.map()
+// Generate random freelancer at an irregular pace
+const addFreelancerIntervalId = setInterval(getNewFreelancer, 1000);
